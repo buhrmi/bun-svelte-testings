4e834af024a84b3f9b5e5cd6221f3cf1021f226d
Stefan Buhrmester <buhrmi@gmail.com>
2024-03-27 23:03:01 +0100

    minimal repo

---
+++ helloworld.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  console.log('asdasd')
+</script>
+
+<script>
+  console.log('adasd')
+  let name = 'World';
+</script>
+
+<h1>
+  Hello {name}!
+</h1>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
---
+++ render.js
@@ -0,0 +1,5 @@
+import './bun-plugin-svelte.js'
+import Hello from './helloworld.svelte'
+
+console.log('Hello is a', typeof Hello)
+// console.log(Hello.render())
